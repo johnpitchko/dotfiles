826392c6616cb5c6d78866e67311d9bde44bc62e
John Pitchko <john.pitchko@icloud.com>
2021-11-26 21:02:34 -0700

    Fix install script

--- install.zsh
+++ install.zsh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-config_dir_dotfiles=/Users/johnpitchko/.dotfiles/.config
-config_dir=/Users/johnpitchko/.config
-config_dir_backup=/Users/johnpitchko/.config.bak
+config_dir_dotfiles=/Users/johnpitchko/.dotfiles/config/nvim
+config_dir=/Users/johnpitchko/.config/nvim
+config_dir_backup=/Users/johnpitchko/.config/nvim.bak
 
 # Make a backup of the existing config directory
 echo "Backup up $config_dir to $config_dir_backup..."
-cp -R -v $config_dir_backup
+mv -v $config_dir $config_dir_backup
 
 # Symlink the config folder in the repo to the home directory
 echo "Symlinking $config_dir to $config_dir_backup..."
