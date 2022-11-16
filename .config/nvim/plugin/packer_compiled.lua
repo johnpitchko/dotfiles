83f1d722a9f9acb6a3f87d1b34b073afecebc48f
John Pitchko <john.pitchko@icloud.com>
2022-11-16 09:03:05 -0700

    Add Comment Frame plugin

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -164,6 +164,12 @@ _G.packer_plugins = {
     path = "/Users/johnpitchko/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-comment-frame"] = {
+    config = { "\27LJ\2\n@\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\23nvim-comment-frame\frequire\0" },
+    loaded = true,
+    path = "/Users/johnpitchko/.local/share/nvim/site/pack/packer/start/nvim-comment-frame",
+    url = "https://github.com/s1n7ax/nvim-comment-frame"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/Users/johnpitchko/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
@@ -302,6 +308,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: nvim-comment-frame
+time([[Config for nvim-comment-frame]], true)
+try_loadstring("\27LJ\2\n@\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\23nvim-comment-frame\frequire\0", "config", "nvim-comment-frame")
+time([[Config for nvim-comment-frame]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
