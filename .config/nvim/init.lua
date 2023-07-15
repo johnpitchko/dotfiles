40f52137efda26bb4634af6af8ddc395eb006364
John Pitchko <john.pitchko@icloud.com>
2023-07-15 16:00:04 -0600

    Swap default Neovim for Astronvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,23 @@
--- Files are placed in lua/user/ directory to avoid namespace issues.
-require("user.plugins")
-require("user.options")
-require("user.keymaps")
+if vim.loader and vim.fn.has "nvim-0.9.1" == 1 then vim.loader.enable() end
 
-require("user.bufferline")
--- require("user.cmp")
-require("user.colorscheme")
-require("user.comment")
-require("user.gitsigns")
-require("user.lsp")
-require("user.lualine")
-require("user.nvim-tree")
-require("user.treesitter")
-require("user.which-key")
+for _, source in ipairs {
+  "astronvim.bootstrap",
+  "astronvim.options",
+  "astronvim.lazy",
+  "astronvim.autocmds",
+  "astronvim.mappings",
+} do
+  local status_ok, fault = pcall(require, source)
+  if not status_ok then vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault) end
+end
+
+if astronvim.default_colorscheme then
+  if not pcall(vim.cmd.colorscheme, astronvim.default_colorscheme) then
+    require("astronvim.utils").notify(
+      ("Error setting up colorscheme: `%s`"):format(astronvim.default_colorscheme),
+      vim.log.levels.ERROR
+    )
+  end
+end
+
+require("astronvim.utils").conditional_func(astronvim.user_opts("polish", nil, false), true)
--- .config/nvim/legacy_vimscript/plugins.vim
+++
@@ -1,6 +0,0 @@
-source $HOME/.config/nvim/plugins/ale.vim
-source $HOME/.config/nvim/plugins/fzf.vim
-source $HOME/.config/nvim/plugins/rg.vim
-source $HOME/.config/nvim/plugins/nerdtree.vim
-source $HOME/.config/nvim/plugins/vim-test.vim
-source $HOME/.config/nvim/plugins/nvim-lspconfig.vim
