956cd1683ab9af22a7883a8af7bafcd44fa7a6a2
John Pitchko <john.pitchko@icloud.com>
2023-10-20 14:06:43 -0600

    Allow single blank line in ERB files

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,12 @@ return {
       formatters_by_ft = {
         eruby = { "htmlbeautifier" },
       },
+      formatters = {
+        htmlbeautifier = {
+          -- Details about CLI args in https://github.com/threedaymonk/htmlbeautifier/blob/master/bin/htmlbeautifier
+          args = { "--keep-blank-lines", "1" },
+        },
+      },
     },
   },
 }
