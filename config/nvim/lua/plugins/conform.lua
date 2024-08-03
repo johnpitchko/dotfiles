9475880d24231eea2ffa8a3c0253022d0806d9c3
John Pitchko <john.pitchko@icloud.com>
2024-08-03 10:42:02 -0600

    Specify htmlbeautifier as the eruby formatter

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -3,8 +3,14 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-
-        -- ruby = { "rubocop" },
+        eruby = { "htmlbeautifier" },
+      },
+      formatters = {
+        htmlbeautifier = {
+          -- Details about CLI args in https://github.com/threedaymonk/htmlbeautifier/blob/master/bin/htmlbeautifier
+          -- keep-blank-lines: default is 0 which eliminates _all_ blank lines in ERB files, which IMHO makes it visually less appealing when coding.
+          args = { "--keep-blank-lines", "1" },
+        },
       },
     },
   },
