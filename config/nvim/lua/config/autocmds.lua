cd7125881b0f6b46568788d35c7884802c120100
John Pitchko <john.pitchko@icloud.com>
2024-11-10 20:31:42 -0700

    Enable format on save

---
+++ config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,13 @@
+-- Format on save
+-- Source: https://www.mitchellhanberg.com/modern-format-on-save-in-neovim/
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
