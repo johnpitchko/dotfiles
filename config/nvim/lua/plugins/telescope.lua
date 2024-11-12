4ce2b27f000041bbc3c21cb2b6679db885e6b6d4
John Pitchko <john.pitchko@icloud.com>
2024-11-12 13:26:15 -0700

    Squash merge add-telescope into minimal-neovim

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,32 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim', 
+    'BurntSushi/ripgrep',
+    'nvim-telescope/telescope-fzf-native.nvim'  -- Recommended to improve search performance
+  },
+  keys = {
+    { "<leader>f", group = "telescope", desc = "Telescope" },
+    { 
+      "<leader>ff", 
+      function()
+        require('telescope.builtin').find_files()
+      end,
+      desc = 'Telescope find files' 
+    },
+    {
+      "<leader>fg",
+      function()
+        require('telescope.builtin').live_grep()
+      end,
+      desc = 'Telescope live grep'
+    },
+    {
+      "<leader>fb",
+      function()
+        require('telescope.builtin').buffers()
+      end,
+      desc = 'Telescope buffers'
+    }
+    -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+  }
+}
