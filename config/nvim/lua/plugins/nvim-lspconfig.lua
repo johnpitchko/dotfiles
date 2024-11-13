7aeb01d7c3f142335d563290d7a7da66440371da
John Pitchko <john.pitchko@icloud.com>
2024-11-12 21:55:57 -0700

    Add HTML LSP

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,14 +1,19 @@
+-- Available LSPs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+
 return {
   "neovim/nvim-lspconfig",
   enabled = true,
   opts = {},
   config = function()
     local lspconfig = require('lspconfig')
+
     lspconfig.ruby_lsp.setup({
       init_options = {
         formatter = 'rubocop',
         linters = { 'rubocop' },
       },
     })
+
+    lspconfig.html.setup({})
   end
 }
