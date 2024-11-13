e51aef1d389bc47ed0db19dc4663e86a9cddb239
John Pitchko <john.pitchko@icloud.com>
2024-11-13 10:43:41 -0700

    Add htmlbeautifier
    And allow one blank space between lines

--- config/nvim/lua/plugins/none_ls.lua
+++ config/nvim/lua/plugins/none_ls.lua
@@ -1,3 +1,9 @@
+-- Note that the _original_ plugin is called `null-ls`, but the author stopped active maintenance.
+-- So it was forked by a new active maintainer and is now called `none-ls`. However,
+-- all internal references, namespaces, variable names, etc.. still use `null-ls`.
+--
+-- Built-in sources: https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+-- Use `:NullLsInfo` to show which formatters are attached to the buffer
 return {
   "nvimtools/none-ls.nvim",
   init = function()
@@ -6,6 +12,9 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.htmlbeautifier.with({
+          extra_args = { "-b", "1" }
+        }),
         -- null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
         -- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
