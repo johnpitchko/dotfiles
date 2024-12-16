84775daea7e4edb008028e5e328b0bdce07f1ce1
John Pitchko <john.pitchko@icloud.com>
2024-12-16 10:01:10 -0700

    Enable fuzzy telescope searches

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,17 +1,17 @@
 return {
   'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 
+  dependencies = {
+    'nvim-lua/plenary.nvim',
     'BurntSushi/ripgrep',
-    'nvim-telescope/telescope-fzf-native.nvim'  -- Recommended to improve search performance
   },
   keys = {
     { "<leader>f", group = "telescope", desc = "Telescope" },
-    { 
-      "<leader>ff", 
+    {
+      "<leader>ff",
       function()
         require('telescope.builtin').find_files()
       end,
-      desc = 'Telescope find files' 
+      desc = 'Telescope find files'
     },
     {
       "<leader>fg",
@@ -28,5 +28,5 @@ return {
       desc = 'Telescope buffers'
     }
     -- vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  }
+  },
 }
