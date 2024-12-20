7ddb3a7285d41ce9b3380f382cb9028379a9b9da
John Pitchko <john.pitchko@icloud.com>
2024-12-19 21:49:00 -0700

    Add lualine

---
+++ config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,8 @@
+-- A blazing fast and easy to configure Neovim statusline written in Lua.
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup()
+  end
+}
